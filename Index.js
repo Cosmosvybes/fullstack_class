1e0a2ac75e67ecac0eb89dfa5a2b891e233e3bbf
cosmos <alfredchrisayo@gmail.com>
2025-07-18 04:19:30 +0000

    added lorem paragraph

---
+++ Index.js
@@ -0,0 +1,7 @@
+// create an array of 20 elements of the following data types , number, strings, boolean, float, object
+//create an object using VSL ICT academy specify every departments  and  use anything  as the corresponding values
+// create an object using Nigeria numbers of states and local gorvernment as the properties.
+var a = true || false;
+var b = true && false;
+
+console.log(!(a && b));
